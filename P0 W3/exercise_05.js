d2169bb1609b6090b2a14776ea0ee5c5a338be38
risanto <potallegta@gmail.com>
2019-08-05 18:08:43 +0700

    exercise 5

---
+++ P0 W3/exercise_05.js
@@ -0,0 +1,26 @@
+// Palindrome
+  
+  function palindrome(kata) {
+
+    var kataBalik = '';
+    
+      for (var i = kata.length - 1; i >= 0; i--) {
+        
+        kataBalik += kata [i];
+      }
+  
+    if (kataBalik === kata) {
+      return true;
+    
+    } else {
+      return false;
+    }
+
+  }
+
+// TEST CASES
+console.log(palindrome('katak')); // true
+console.log(palindrome('blanket')); // false
+console.log(palindrome('civic')); // true
+console.log(palindrome('kasur rusak')); // true
+console.log(palindrome('mister')); // false
