b4cbe9939d7f4aa54dbc49f54f4e24269ee35bc3
risanto <potallegta@gmail.com>
2019-08-05 15:51:50 +0700

    exercise 3

--- P0 W3/exercise_03.js
+++ P0 W3/exercise_03.js
@@ -1,25 +1,5 @@
 // Tantangan Array 2 (Melooping menggunakan Array)
 
-// Nomor ID:  0001
-// Nama Lengkap:  Roman Alamsyah
-// TTL:  Bandar Lampung 21/05/1989
-// Hobi:  Membaca
-
-// Nomor ID:  0002
-// Nama Lengkap:  Dika Sembiring
-// TTL:  Medan 10/10/1992
-// Hobi:  Bermain Gitar
-
-// Nomor ID:  0003
-// Nama Lengkap:  Winona
-// TTL:  Ambon 25/12/1965
-// Hobi:  Memasak
-
-// Nomor ID:  0004
-// Nama Lengkap:  Bintang Senjaya
-// TTL:  Martapura 6/4/1970
-// Hobi:  Berkebun
-
 function dataHandling (n) {
   
   var input = [
