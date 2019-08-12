d18093d666c0ea4d8fb2c874d717404833fc1ebe
risanto <potallegta@gmail.com>
2019-08-12 17:24:34 +0700

    add exercise 7: urutkan abjad

---
+++ P0 W4/exercise_07.js
@@ -0,0 +1,12 @@
+function urutkanAbjad(str) {
+  var strArr = str.split('');
+  strArr = strArr.sort().join('');
+  return strArr;
+}
+
+// TEST CASES
+console.log(urutkanAbjad('hello')); // 'ehllo'
+console.log(urutkanAbjad('truncate')); // 'acenrttu'
+console.log(urutkanAbjad('developer')); // 'deeeloprv'
+console.log(urutkanAbjad('software')); // 'aeforstw'
+console.log(urutkanAbjad('aegis')); // 'aegis'
