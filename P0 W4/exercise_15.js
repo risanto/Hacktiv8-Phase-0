42badd014caa0558b88cdb1909d3b9fa4f12dd04
risanto <potallegta@gmail.com>
2019-08-16 13:14:48 +0700

    add exercise 15: highest score

---
+++ P0 W4/exercise_15.js
@@ -0,0 +1,94 @@
+// Highest Score
+
+function highestScore (students) {
+
+  var res = {};
+  
+  for (i = 0; i < students.length; i++) {
+    var tempClass = students[i].class;
+    var tempName = students[i].name;
+    var tempScore = students[i].score;
+
+    for (j = 0; j < students.length; j++) {
+      if (students[j].class === tempClass && students[j].score > tempScore) {
+        tempClass = students[j].class;
+        tempScore = students[j].score;
+        tempName = students[j].name;
+      }
+    }
+
+    res[tempClass] = {
+      name: tempName,
+      score: tempScore
+    }
+  }
+  console.log ("============================")
+  return res;
+}
+
+// TEST CASE
+console.log(highestScore([
+  {
+    name: 'Dimitri',
+    score: 90,
+    class: 'foxes'
+  },
+  {
+    name: 'Alexei',
+    score: 85,
+    class: 'wolves'
+  },
+  {
+    name: 'Sergei',
+    score: 74,
+    class: 'foxes'
+  },
+  {
+    name: 'Anastasia',
+    score: 78,
+    class: 'wolves'
+  }
+]));
+
+// {
+//   foxes: { name: 'Dimitri', score: 90 },
+//   wolves: { name: 'Alexei', score: 85 }
+// }
+
+
+console.log(highestScore([
+  {
+    name: 'Alexander',
+    score: 100,
+    class: 'foxes'
+  },
+  {
+    name: 'Alisa',
+    score: 76,
+    class: 'wolves'
+  },
+  {
+    name: 'Vladimir',
+    score: 92,
+    class: 'foxes'
+  },
+  {
+    name: 'Albert',
+    score: 71,
+    class: 'wolves'
+  },
+  {
+    name: 'Viktor',
+    score: 80,
+    class: 'tigers'
+  }
+]));
+
+// {
+//   foxes: { name: 'Alexander', score: 100 },
+//   wolves: { name: 'Alisa', score: 76 },
+//   tigers: { name: 'Viktor', score: 80 }
+// }
+
+
+console.log(highestScore([])); //{}
