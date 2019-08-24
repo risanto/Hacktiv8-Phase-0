8502e711f10e505a61ddee7ba82ebc9395487483
risanto <potallegta@gmail.com>
2019-08-24 10:48:06 +0700

    exc 17: kali terus rekursif

--- P0 W5/exercise_17.js
+++ P0 W5/exercise_17.js
@@ -1,32 +1,36 @@
 // Kali Terus (Rekursif)
 
 function kaliTerusRekursif(angka) {
-  
-  var angkaStr = String(angka);
 
-  if (angkaStr.length === 1) {
+  if (String(angka).length === 1) {
     return angka;
   }
-
-  var angkaSplit = angkaStr.split();
-
-  console.log (angkaSplit)
-
-  res = Number(angkaStr[0]) * Number(angkaStr.slice(1)[0]);
-
-  console.log (res);
-
-  if (String(res).length === 1) {
-    return res;
-  }
   else {
-    return kaliTerusRekursif(res)
-  }
+
+    // split manual
+
+    var strSplit = [];
+
+    var angkaStr = String(angka);
+
+    for (i = 0; i < angkaStr.length; i++) {
+      strSplit.push(angkaStr[i]);
+    }
+
+    // perkalian
+
+    temp = strSplit[0];
+
+    for (i = 1; i < strSplit.length; i++) {
+      temp *= strSplit[i];
+    }
+    return kaliTerusRekursif(temp);
+  }  
 }
 
 // TEST CASES
-// console.log(kaliTerusRekursif(66)); // 8
-// console.log(kaliTerusRekursif(3)); // 3
-// console.log(kaliTerusRekursif(24)); // 8
-// console.log(kaliTerusRekursif(654)); // 0
+console.log(kaliTerusRekursif(66)); // 8
+console.log(kaliTerusRekursif(3)); // 3
+console.log(kaliTerusRekursif(24)); // 8
+console.log(kaliTerusRekursif(654)); // 0
 console.log(kaliTerusRekursif(1231)); // 6
